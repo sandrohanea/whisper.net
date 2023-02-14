dfabcb85162b23abb5127368f9d1c5f395d63617
Adam Novak <adamnovak@microsoft.com>
2023-02-14 13:22:52 +0100

    Refactored library loader

---
+++ Whisper.net/Native/LibraryLoader/ILibraryLoader.cs
@@ -0,0 +1,8 @@
+﻿// © Microsoft Corporation. All rights reserved.
+
+namespace Whisper.net.Native.LibraryLoader;
+
+internal interface ILibraryLoader
+{
+    IntPtr OpenLibrary(string filename, int flags);
+}
---
+++ Whisper.net/Native/LibraryLoader/PosixLibraryLoader.cs
@@ -0,0 +1,34 @@
+﻿// © Microsoft Corporation. All rights reserved.
+
+using System.Runtime.InteropServices;
+
+namespace Whisper.net.Native.LibraryLoader;
+
+internal class PosixLibraryLoader : ILibraryLoader
+{
+	[DllImport("libdl.so", ExactSpelling = true, CharSet = CharSet.Auto)]
+	public static extern IntPtr dlopen(string filename, int flags);
+
+	public IntPtr OpenLibrary(string filename, int flags)
+	{
+		try
+		{
+			return PosixLibraryLoader2.dlopen(filename, flags);
+		}
+		catch
+		{
+			return dlopen(filename, flags);
+		}
+	}
+
+	private class PosixLibraryLoader2 : ILibraryLoader
+	{
+		[DllImport("libdl.so.2", ExactSpelling = true, CharSet = CharSet.Auto)]
+		public static extern IntPtr dlopen(string filename, int flags);
+
+		public IntPtr OpenLibrary(string filename, int flags)
+		{
+			return dlopen(filename, flags);
+		}
+	}
+}
