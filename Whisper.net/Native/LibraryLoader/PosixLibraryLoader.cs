d262d1ec745e0de7e00574be6f863c227ea2848b
Adam Novak <adamnovak@microsoft.com>
2023-02-14 13:33:42 +0100

    Refactoring

--- Whisper.net/Native/LibraryLoader/PosixLibraryLoader.cs
+++ Whisper.net/Native/LibraryLoader/PosixLibraryLoader.cs
@@ -7,20 +7,20 @@ namespace Whisper.net.Native.LibraryLoader;
 internal class PosixLibraryLoader : ILibraryLoader
 {
 	[DllImport("libdl.so", ExactSpelling = true, CharSet = CharSet.Auto, EntryPoint = "dlopen")]
-	public static extern IntPtr OpenLibdl(string filename, int flags);
+	public static extern IntPtr NativeOpenLibraryLibdl(string filename, int flags);
 
 	[DllImport("libdl.so.2", ExactSpelling = true, CharSet = CharSet.Auto, EntryPoint = "dlopen")]
-	public static extern IntPtr OpenLibdl2 (string filename, int flags);
+	public static extern IntPtr NativeOpenLibraryLibdl2 (string filename, int flags);
 
 	public IntPtr OpenLibrary(string filename, int flags)
 	{
 		try
 		{
-			return OpenLibdl(filename, flags);
+			return NativeOpenLibraryLibdl2(filename, flags);
 		}
 		catch
 		{
-			return OpenLibdl2(filename, flags);
+			return NativeOpenLibraryLibdl(filename, flags);
 		}
 	}
 }
