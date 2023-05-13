8ad27a5c3a6bd385fcf9f2bb3c3e08c6f2bbe53a
Sandro Hanea <sandro@shane.ro>
2023-05-13 15:01:45 +0200

    Fixed #35 Wave parsing where junk chunk is provided before fmt chunk.

--- Whisper.net/Wave/WaveParser.cs
+++ Whisper.net/Wave/WaveParser.cs
@@ -129,6 +129,7 @@ public sealed class WaveParser
         }
         return samples;
     }
+
     private void Initialize()
     {
         if (wasInitialized)
@@ -136,15 +137,14 @@ public sealed class WaveParser
             return;
         }
 
-        var buffer = new byte[36];
-
-        //Read RIFF Header
-        var actualRead = waveStream.Read(buffer, 0, 36);
-        if (actualRead != 36)
+        var buffer = new byte[12];
+        var actualRead = waveStream.Read(buffer, 0, 12);
+        if (actualRead != 12)
         {
             throw new CorruptedWaveException("Invalid wave file, the size is too small.");
         }
 
+        //Read RIFF Header
         if (buffer[0] != 'R' || buffer[1] != 'I' || buffer[2] != 'F' || buffer[3] != 'F')
         {
             throw new CorruptedWaveException("Invalid wave file RIFF header.");
@@ -153,51 +153,91 @@ public sealed class WaveParser
         // Skip FileSize 4 => 8
 
         // Read Wave and Fmt tags
-        if (buffer[8] != 'W' || buffer[9] != 'A' || buffer[10] != 'V' || buffer[11] != 'E' || buffer[12] != 'f' || buffer[13] != 'm' || buffer[14] != 't' || buffer[15] != ' ')
+        if (buffer[8] != 'W' || buffer[9] != 'A' || buffer[10] != 'V' || buffer[11] != 'E')
         {
             throw new CorruptedWaveException("Invalid wave file header.");
         }
 
-        // Read Format Chunk Size
-        var formatChunkSize = BitConverter.ToInt32(buffer, 16);
-        if (formatChunkSize < 0)
+        // Search for format chunk
+        int fmtChunkSize;
+        while (true)
+        {
+            var nextChunkHeader = new byte[8];
+            actualRead = waveStream.Read(nextChunkHeader, 0, 8);
+            if (actualRead != 8)
+            {
+                throw new CorruptedWaveException("Invalid wave file, cannot read next chunk.");
+            }
+
+            var chunkSize = BitConverter.ToInt32(nextChunkHeader, 4);
+            if (chunkSize < 0)
+            {
+                throw new CorruptedWaveException("Invalid wave chunk size.");
+            }
+
+            if (nextChunkHeader[0] == 'f' && nextChunkHeader[1] == 'm' && nextChunkHeader[2] == 't' && nextChunkHeader[3] == ' ')
+            {
+                fmtChunkSize = chunkSize;
+                break;
+            }
+
+            if (waveStream.CanSeek)
+            {
+                waveStream.Seek(chunkSize, SeekOrigin.Current);
+            }
+            else
+            {
+                var restOfChunk = new byte[chunkSize];
+                waveStream.Read(restOfChunk, 0, chunkSize);
+            }
+        }
+
+        if (fmtChunkSize < 16)
         {
             throw new CorruptedWaveException("Invalid wave format size.");
         }
+
+        var fmtBuffer = new byte[fmtChunkSize];
+        actualRead = waveStream.Read(fmtBuffer, 0, fmtChunkSize);
+        if (actualRead != fmtChunkSize)
+        {
+            throw new CorruptedWaveException("Invalid wave file, cannot read format chunk.");
+        }
+
         // Read Format
-        var format = BitConverter.ToUInt16(buffer, 20);
+        var format = BitConverter.ToUInt16(fmtBuffer, 0);
         if (format != 1)
         {
             throw new CorruptedWaveException("Unsupported wave file");
         }
 
-        channels = BitConverter.ToUInt16(buffer, 22);
-        sampleRate = BitConverter.ToUInt32(buffer, 24);
+        channels = BitConverter.ToUInt16(fmtBuffer, 2);
+        sampleRate = BitConverter.ToUInt32(fmtBuffer, 4);
         if (sampleRate != 16000)
         {
             throw new NotSupportedWaveException("Only 16KHz sample rate is supported.");
         }
 
-        // Skip Average bytes rate 28 => 32
+        // Skip Average bytes rate 8 => 12
 
-        // Skip Block Allign 32 => 34
+        // Skip Block Allign 12 => 14
 
-        bitsPerSample = BitConverter.ToUInt16(buffer, 34);
+        bitsPerSample = BitConverter.ToUInt16(fmtBuffer, 14);
         if (bitsPerSample != 16)
         {
             throw new NotSupportedWaveException("Only 16 bits per sample is supported.");
         }
-        // Until now we have read 16 bytes in format, the rest is cbSize, averageBytesRate, and is ignored for now.
-        if (formatChunkSize > 16)
+        // Until now we have read 18 bytes in format, the rest is cbSize, averageBytesRate, and is ignored for now.
+        if (fmtChunkSize > 18)
         {
             if (waveStream.CanSeek)
             {
-                waveStream.Seek(formatChunkSize - 16, SeekOrigin.Current);
+                waveStream.Seek(fmtChunkSize - 18, SeekOrigin.Current);
             }
             else
             {
-                var restOfBuffer = new byte[formatChunkSize - 16];
-                waveStream.Read(restOfBuffer, 0, formatChunkSize - 16);
+                var restOfBuffer = new byte[fmtChunkSize - 18];
+                waveStream.Read(restOfBuffer, 0, fmtChunkSize - 18);
             }
         }
 
@@ -240,10 +280,9 @@ public sealed class WaveParser
             return;
         }
 
-        var buffer = new byte[36];
-
-        var actualRead = waveStream.Read(buffer, 0, 36);
-        if (actualRead != 36)
+        var buffer = new byte[12];
+        var actualRead = await waveStream.ReadAsync(buffer, 0, 12);
+        if (actualRead != 12)
         {
             throw new CorruptedWaveException("Invalid wave file, the size is too small.");
         }
@@ -257,51 +296,91 @@ public sealed class WaveParser
         // Skip FileSize 4 => 8
 
         // Read Wave and Fmt tags
-        if (buffer[8] != 'W' || buffer[9] != 'A' || buffer[10] != 'V' || buffer[11] != 'E' || buffer[12] != 'f' || buffer[13] != 'm' || buffer[14] != 't' || buffer[15] != ' ')
+        if (buffer[8] != 'W' || buffer[9] != 'A' || buffer[10] != 'V' || buffer[11] != 'E')
         {
             throw new CorruptedWaveException("Invalid wave file header.");
         }
 
-        // Read Format Chunk Size
-        var formatChunkSize = BitConverter.ToInt32(buffer, 16);
-        if (formatChunkSize < 0)
+        // Search for format chunk
+        int fmtChunkSize;
+        while (true)
+        {
+            var nextChunkHeader = new byte[8];
+            actualRead = await waveStream.ReadAsync(nextChunkHeader, 0, 8);
+            if (actualRead != 8)
+            {
+                throw new CorruptedWaveException("Invalid wave file, cannot read next chunk.");
+            }
+
+            var chunkSize = BitConverter.ToInt32(nextChunkHeader, 4);
+            if (chunkSize < 0)
+            {
+                throw new CorruptedWaveException("Invalid wave chunk size.");
+            }
+
+            if (nextChunkHeader[0] == 'f' && nextChunkHeader[1] == 'm' && nextChunkHeader[2] == 't' && nextChunkHeader[3] == ' ')
+            {
+                fmtChunkSize = chunkSize;
+                break;
+            }
+
+            if (waveStream.CanSeek)
+            {
+                waveStream.Seek(chunkSize, SeekOrigin.Current);
+            }
+            else
+            {
+                var restOfChunk = new byte[chunkSize];
+                await waveStream.ReadAsync(restOfChunk, 0, chunkSize);
+            }
+        }
+
+        if (fmtChunkSize < 16)
         {
             throw new CorruptedWaveException("Invalid wave format size.");
         }
+
+        var fmtBuffer = new byte[fmtChunkSize];
+        actualRead = await waveStream.ReadAsync(fmtBuffer, 0, fmtChunkSize);
+        if (actualRead != fmtChunkSize)
+        {
+            throw new CorruptedWaveException("Invalid wave file, cannot read format chunk.");
+        }
+
         // Read Format
-        var format = BitConverter.ToUInt16(buffer, 20);
+        var format = BitConverter.ToUInt16(fmtBuffer, 0);
         if (format != 1)
         {
             throw new CorruptedWaveException("Unsupported wave file");
         }
 
-        channels = BitConverter.ToUInt16(buffer, 22);
-        sampleRate = BitConverter.ToUInt32(buffer, 24);
+        channels = BitConverter.ToUInt16(fmtBuffer, 2);
+        sampleRate = BitConverter.ToUInt32(fmtBuffer, 4);
         if (sampleRate != 16000)
         {
             throw new NotSupportedWaveException("Only 16KHz sample rate is supported.");
         }
 
-        // Skip Average bytes rate 28 => 32
+        // Skip Average bytes rate 8 => 12
 
-        // Skip Block Allign 32 => 34
+        // Skip Block Allign 12 => 14
 
-        bitsPerSample = BitConverter.ToUInt16(buffer, 34);
+        bitsPerSample = BitConverter.ToUInt16(fmtBuffer, 14);
         if (bitsPerSample != 16)
         {
             throw new NotSupportedWaveException("Only 16 bits per sample is supported.");
         }
-        // Until now we have read 16 bytes in format, the rest is cbSize, averageBytesRate, and is ignored for now.
-        if (formatChunkSize > 16)
+        // Until now we have read 18 bytes in format, the rest is cbSize, averageBytesRate, and is ignored for now.
+        if (fmtChunkSize > 18)
         {
             if (waveStream.CanSeek)
             {
-                waveStream.Seek(formatChunkSize - 16, SeekOrigin.Current);
+                waveStream.Seek(fmtChunkSize - 18, SeekOrigin.Current);
             }
             else
             {
-                var restOfBuffer = new byte[formatChunkSize - 16];
-                await waveStream.ReadAsync(restOfBuffer, 0, formatChunkSize - 16);
+                var restOfBuffer = new byte[fmtChunkSize - 18];
+                await waveStream.ReadAsync(restOfBuffer, 0, fmtChunkSize - 18);
             }
         }
 
