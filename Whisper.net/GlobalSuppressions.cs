bc29e8cd088b8859482945521041a2809a565cf7
Sandro Hanea <sandro@shane.ro>
2023-05-20 17:29:02 +0200

    Changed supression source.

--- Whisper.net/GlobalSuppressions.cs
+++ Whisper.net/GlobalSuppressions.cs
@@ -8,5 +8,5 @@
 using System.Diagnostics.CodeAnalysis;
 
 // Suppressed these because the Memory based oberload is not available in .NET Standard 2.0, which is one of the target frameworks.
-[assembly: SuppressMessage("Performance", "CA1835:Prefer the 'Memory'-based overloads for 'ReadAsync' and 'WriteAsync'", Justification = "<Pending>", Scope = "member", Target = "~M:Whisper.net.Wave.WaveParser.InitializeAsync~System.Threading.Tasks.Task")]
 [assembly: SuppressMessage("Performance", "CA1835:Prefer the 'Memory'-based overloads for 'ReadAsync' and 'WriteAsync'", Justification = "<Pending>", Scope = "member", Target = "~M:Whisper.net.Wave.WaveParser.GetAvgSamplesAsync(System.Threading.CancellationToken)~System.Threading.Tasks.Task{System.Single[]}")]
+[assembly: SuppressMessage("Performance", "CA1835:Prefer the 'Memory'-based overloads for 'ReadAsync' and 'WriteAsync'", Justification = "<Pending>", Scope = "member", Target = "~M:Whisper.net.Wave.WaveParser.InitializeCore(System.Boolean)~System.Threading.Tasks.Task")]
