716eb27335fc6444f1f49f5f59af3850f7d219f2
Sandro Hanea <sandro@shane.ro>
2023-06-03 17:44:36 +0200

    Improved model download + added quantized support + added coreml downloader

---
+++ Whisper.net/Ggml/QuantizationType.cs
@@ -0,0 +1,13 @@
+// Licensed under the MIT license: https://opensource.org/licenses/MIT
+
+namespace Whisper.net.Ggml;
+public enum QuantizationType
+{
+    NoQuantization,
+    Q4_0,
+    Q4_1,
+    Q4_2,
+    Q5_0,
+    Q5_1,
+    Q8_0
+}
