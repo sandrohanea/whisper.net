09ae26a7e76b50a9f6512288a75ac8388c207702
sandrohanea <40202887+sandrohanea@users.noreply.github.com>
2023-06-04 17:42:38 +0200

    Improved model download + added quantized support + added coreml downloader (#61)
    * Improved model download + added quantized support + added coreml downloader
    
    * No quantization for CoreML encoder yet.
    
    * Added Quantization test + changed downloader
    
    * Added quantized test
    
    * Added CoreML Example

---
+++ Whisper.net.Tests/ProcessQuantizedTests.cs
@@ -0,0 +1,54 @@
+// Licensed under the MIT license: https://opensource.org/licenses/MIT
+
+using FluentAssertions;
+using NUnit.Framework;
+using Whisper.net.Ggml;
+
+namespace Whisper.net.Tests;
+
+public class ProcessQuantizedTests
+{
+    private string ggmlModelPath = string.Empty;
+
+    [OneTimeSetUp]
+    public async Task SetupAsync()
+    {
+        ggmlModelPath = Path.GetTempFileName();
+        var model = await WhisperGgmlDownloader.GetGgmlModelAsync(GgmlType.Tiny, QuantizationType.Q5_0);
+        using var fileWriter = File.OpenWrite(ggmlModelPath);
+        await model.CopyToAsync(fileWriter);
+    }
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        File.Delete(ggmlModelPath);
+    }
+
+    [Test]
+    public void TestHappyFlowQuantized()
+    {
+        var segments = new List<SegmentData>();
+        var progress = new List<int>();
+        var encoderBegins = new List<EncoderBeginData>();
+        using var factory = WhisperFactory.FromPath(ggmlModelPath);
+        using var processor = factory.CreateBuilder()
+                        .WithLanguage("en")
+                        .WithEncoderBeginHandler((e) =>
+                        {
+                            encoderBegins.Add(e);
+                            return true;
+                        })
+                        .WithProgressHandler(progress.Add)
+                        .WithSegmentEventHandler(segments.Add)
+                        .Build();
+
+        using var fileReader = File.OpenRead("bush.wav");
+        processor.Process(fileReader);
+
+        segments.Should().HaveCountGreaterThan(0);
+        encoderBegins.Should().HaveCountGreaterThanOrEqualTo(1);
+        progress.Should().BeInAscendingOrder().And.HaveCountGreaterThan(1);
+
+        segments.Should().Contain(segmentData => segmentData.Text.Contains("My fellow Americans"));
+    }
+}
--- Whisper.net/Ggml/WhisperGgmlDownloader.cs
+++ Whisper.net/Ggml/WhisperGgmlDownloader.cs
@@ -1,36 +1,108 @@
 // Licensed under the MIT license: https://opensource.org/licenses/MIT
 
+using System.IO.Compression;
+
 namespace Whisper.net.Ggml;
 
 public static class WhisperGgmlDownloader
 {
     private static readonly Lazy<HttpClient> httpClient = new(() => new HttpClient() { Timeout = Timeout.InfiniteTimeSpan });
 
-    public static async Task<Stream> GetGgmlModelAsync(GgmlType type, CancellationToken cancellationToken = default)
+    /// <summary>
+    /// Gets the download stream for the model
+    /// </summary>
+    /// <param name="type">The type of the model which needs to be downloaded.</param>
+    /// <param name="quantization">The quantization of the model.</param>
+    /// <param name="cancellationToken">A cancellation token used to cancell the request to huggingface.</param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public static async Task<Stream> GetGgmlModelAsync(GgmlType type, QuantizationType quantization = QuantizationType.NoQuantization, CancellationToken cancellationToken = default)
     {
-        var url = type switch
-        {
-            GgmlType.Tiny => "https://huggingface.co/datasets/ggerganov/whisper.cpp/resolve/main/ggml-tiny.bin",
-            GgmlType.TinyEn => "https://huggingface.co/datasets/ggerganov/whisper.cpp/resolve/main/ggml-tiny.en.bin",
-            GgmlType.Base => "https://huggingface.co/datasets/ggerganov/whisper.cpp/resolve/main/ggml-base.bin",
-            GgmlType.BaseEn => "https://huggingface.co/datasets/ggerganov/whisper.cpp/resolve/main/ggml-base.en.bin",
-            GgmlType.Small => "https://huggingface.co/datasets/ggerganov/whisper.cpp/resolve/main/ggml-small.bin",
-            GgmlType.SmallEn => "https://huggingface.co/datasets/ggerganov/whisper.cpp/resolve/main/ggml-small.en.bin",
-            GgmlType.Medium => "https://huggingface.co/datasets/ggerganov/whisper.cpp/resolve/main/ggml-medium.bin",
-            GgmlType.MediumEn => "https://huggingface.co/datasets/ggerganov/whisper.cpp/resolve/main/ggml-medium.en.bin",
-            GgmlType.LargeV1 => "https://huggingface.co/datasets/ggerganov/whisper.cpp/resolve/main/ggml-large-v1.bin",
-            GgmlType.Large => "https://huggingface.co/datasets/ggerganov/whisper.cpp/resolve/main/ggml-large.bin",
-            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
-        };
+        var subdirectory = GetQuantizationSubdirectory(quantization);
+        var modelName = GetModelName(type);
+
+        var url = $"https://huggingface.co/sandrohanea/whisper.net/resolve/v1/{subdirectory}/{modelName}.bin";
 
         var request = new HttpRequestMessage(HttpMethod.Get, url);
         var response = await httpClient.Value.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
         response.EnsureSuccessStatusCode();
 
-#if IOS || MACCATALYST || TVOS || ANDROID || MACOS
-        return await response.Content.ReadAsStreamAsync(cancellationToken);
-#else
+#if NETSTANDARD
         return await response.Content.ReadAsStreamAsync();
+#else
+        return await response.Content.ReadAsStreamAsync(cancellationToken);
 #endif
+    }
+
+    /// <summary>
+    /// Gets the download stream for the CoreML model, which is a zip file.
+    /// </summary>
+    /// <param name="type">The type of the model which needs to be downloaded.</param>
+    /// <param name="cancellationToken">A cancellation token used to cancell the request to huggingface.</param>
+    /// <remarks>
+    /// Needs to be extracted on in the same directory as the ggml model, also ggml model needs to be loaded using file path, not stream.
+    /// </remarks>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public static async Task<Stream> GetEncoderCoreMLModelAsync(GgmlType type, CancellationToken cancellationToken = default)
+    {
+        var modelName = GetModelName(type);
+        var url = $"https://huggingface.co/sandrohanea/whisper.net/resolve/v1/coreml/{modelName}-encoder.zip";
+
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+        var response = await httpClient.Value.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+        response.EnsureSuccessStatusCode();
+
+#if NETSTANDARD
+        return await response.Content.ReadAsStreamAsync();
+#else
+        return await response.Content.ReadAsStreamAsync(cancellationToken);
+#endif
+    }
+
+    /// <summary>
+    /// Extracts the given zip stream to the given path.
+    /// </summary>
+    /// <param name="zipStream">The zip stream to be extracted.</param>
+    /// <param name="path">The path.</param>
+    /// <remarks>
+    /// In order to work, you'll need to provide the same path as the ggml model.
+    /// </remarks>
+    /// <returns></returns>
+    public static async Task ExtractToPath(this Task<Stream> zipStream, string path)
+    {
+        using var zipArchive = new ZipArchive(await zipStream, ZipArchiveMode.Read);
+        zipArchive.ExtractToDirectory(path);
+    }
+
+    private static string GetModelName(GgmlType type)
+    {
+        return type switch
+        {
+            GgmlType.Tiny => "ggml-tiny",
+            GgmlType.TinyEn => "ggml-tiny.en",
+            GgmlType.Base => "ggml-base",
+            GgmlType.BaseEn => "ggml-base.en",
+            GgmlType.Small => "ggml-small",
+            GgmlType.SmallEn => "ggml-small.en",
+            GgmlType.Medium => "ggml-medium",
+            GgmlType.MediumEn => "ggml-medium.en",
+            GgmlType.LargeV1 => "ggml-large-v1",
+            GgmlType.Large => "ggml-large",
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
+
+    private static string GetQuantizationSubdirectory(QuantizationType quantization)
+    {
+        return quantization switch
+        {
+            QuantizationType.NoQuantization => "classic",
+            QuantizationType.Q4_0 => "q4_0",
+            QuantizationType.Q4_1 => "q4_1",
+            QuantizationType.Q4_2 => "q4_2",
+            QuantizationType.Q5_0 => "q5_0",
+            QuantizationType.Q5_1 => "q5_1",
+            QuantizationType.Q8_0 => "q8_0",
+            _ => throw new ArgumentOutOfRangeException(nameof(quantization), quantization, null)
+        };
     }
 }
