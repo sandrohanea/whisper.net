d63f2d721d54f125cb6361b2b63a35ea966fd2ea
Sandro Hanea <sandrohanea@microsoft.com>
2024-10-07 12:00:49 +0200

    Upgrade packages

--- Whisper.net/Internals/ModelLoader/WhisperProcessorModelFileLoader.cs
+++ Whisper.net/Internals/ModelLoader/WhisperProcessorModelFileLoader.cs
@@ -15,19 +15,19 @@ internal sealed class WhisperProcessorModelFileLoader(string pathModel, bool use
     {
         return NativeMethods.whisper_init_from_file_with_params_no_state(pathModel,
             new WhisperContextParams()
-        {
-            UseGpu = useGpu ? (byte)1 : (byte)0,
-            FlashAttention = 0,
-            GpuDevice = 0,
-            DtwTokenLevelTimestamp = 0,
-            HeadsPreset = WhisperAlignmentHeadsPreset.WHISPER_AHEADS_NONE,
-            DtwNTop = -1,
-            WhisperAheads = new WhisperAheads()
             {
-                NHeads = 0,
-                Heads = IntPtr.Zero
-            },
-            Dtw_mem_size = 1024 * 1024 * 128,
-        });
+                UseGpu = useGpu ? (byte)1 : (byte)0,
+                FlashAttention = 0,
+                GpuDevice = 0,
+                DtwTokenLevelTimestamp = 0,
+                HeadsPreset = WhisperAlignmentHeadsPreset.WHISPER_AHEADS_NONE,
+                DtwNTop = -1,
+                WhisperAheads = new WhisperAheads()
+                {
+                    NHeads = 0,
+                    Heads = IntPtr.Zero
+                },
+                Dtw_mem_size = 1024 * 1024 * 128,
+            });
     }
 }
