46210225d6238db91b9d08fa5f3139418c379629
Sandro Hanea <sandro@shane.ro>
2024-10-16 16:25:49 +0200

    Fixed cuda and tested.

--- Whisper.net/LibraryLoader/CudaHelper.cs
+++ Whisper.net/LibraryLoader/CudaHelper.cs
@@ -1,34 +1,45 @@
 // Licensed under the MIT license: https://opensource.org/licenses/MIT
 
-#if NET6_0_OR_GREATER
 using System.Runtime.InteropServices;
-#endif
 using Whisper.net.Internals.Native;
-using Whisper.net.Internals.Native.Implementations;
+using Whisper.net.Internals.Native.Implementations.Cuda;
 
 namespace Whisper.net.LibraryLoader;
 internal static class CudaHelper
 {
     public static bool IsCudaAvailable()
     {
-        INativeCuda nativeCuda;
-#if NET6_0_OR_GREATER
-        if (!NativeLibrary.TryLoad("cuda", out var library))
-        {
-            return false;
-        }
-        nativeCuda = new NativeLibraryCuda(library);
-#else
+        INativeCuda? nativeCuda = null;
         try
         {
-            nativeCuda = new DllImportNativeCuda();
-        }
-        catch
-        {
-            return false;
-        }
+#if NET6_0_OR_GREATER
+            var libName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                               ? DllImportNativeCuda_64_12.LibraryName // Only 64-bit Windows is supported for now
+                               : DllImportNativeLibcuda.LibraryName;
+
+            if (!NativeLibrary.TryLoad(libName, out var library))
+            {
+                return false;
+            }
+            nativeCuda = new NativeLibraryCuda(library);
+#else
+            try
+            {
+                nativeCuda = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                                ? new DllImportNativeCuda_64_12()
+                                : new DllImportNativeLibcuda();
+            }
+            catch
+            {
+                return false;
+            }
 #endif
-        nativeCuda.CudaGetDeviceCount(out var count);
-        return count > 0;
+            nativeCuda.CudaGetDeviceCount(out var count);
+            return count > 0;
+        }
+        finally
+        {
+            nativeCuda?.Dispose();
+        }
     }
 }
