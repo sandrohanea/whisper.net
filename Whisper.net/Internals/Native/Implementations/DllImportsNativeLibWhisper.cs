2160a627b43176a0934eac9cb749602fce89e21e
Sandro Hanea <sandro@shane.ro>
2024-11-26 19:36:00 +0100

    Vulkan wip

--- Whisper.net/Internals/Native/Implementations/DllImportsNativeLibWhisper.cs
+++ Whisper.net/Internals/Native/Implementations/DllImportsNativeLibWhisper.cs
@@ -10,82 +10,79 @@ namespace Whisper.net.Internals.Native.Implementations;
 /// </summary>
 internal class DllImportsNativeLibWhisper : INativeWhisper
 {
-    const string libraryName = "libwhisper";
-    const string ggmlLibraryName = "libggml-whisper";
-
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern IntPtr whisper_init_from_file_with_params_no_state(string path, WhisperContextParams whisperContextParams);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern IntPtr whisper_init_from_buffer_with_params_no_state(IntPtr buffer, nuint buffer_size, WhisperContextParams whisperContextParams);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern void whisper_free(IntPtr context);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern void whisper_free_params(IntPtr paramsPtr);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern IntPtr whisper_full_default_params_by_ref(WhisperSamplingStrategy strategy);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern int whisper_full_with_state(IntPtr context, IntPtr state, WhisperFullParams parameters, IntPtr samples, int nSamples);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern int whisper_full_n_segments_from_state(IntPtr state);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern long whisper_full_get_segment_t0_from_state(IntPtr state, int index);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern long whisper_full_get_segment_t1_from_state(IntPtr state, int index);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern IntPtr whisper_full_get_segment_text_from_state(IntPtr state, int index);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern int whisper_full_n_tokens_from_state(IntPtr state, int index);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern float whisper_full_get_token_p_from_state(IntPtr state, int segmentIndex, int tokenIndex);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern int whisper_lang_max_id();
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern int whisper_lang_auto_detect_with_state(IntPtr context, IntPtr state, int offset_ms, int n_threads, IntPtr lang_probs);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern int whisper_pcm_to_mel_with_state(IntPtr context, IntPtr state, IntPtr samples, int nSamples, int nThreads);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern IntPtr whisper_lang_str(int lang_id);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern IntPtr whisper_init_state(IntPtr context);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern void whisper_free_state(IntPtr state);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern int whisper_full_lang_id_from_state(IntPtr state);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern void whisper_log_set(IntPtr logCallback, IntPtr user_data);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern void whisper_ctx_init_openvino_encoder_with_state(IntPtr context, IntPtr state, IntPtr modelPath, IntPtr device, IntPtr cacheDir);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern WhisperTokenData whisper_full_get_token_data_from_state(IntPtr state, int segmentIndex, int tokenIndex);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern IntPtr whisper_full_get_token_text_from_state(IntPtr context, IntPtr state, int segmentIndex, int tokenIndex);
 
-    [DllImport(ggmlLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibGgmlWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern void ggml_log_set(IntPtr logCallback, IntPtr user_data);
 
-    [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(NativeConstants.LibWhisperLibraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     public static extern IntPtr whisper_print_system_info();
 
     public INativeWhisper.whisper_init_from_file_with_params_no_state Whisper_Init_From_File_With_Params_No_State => whisper_init_from_file_with_params_no_state;
--- Whisper.net/Internals/Native/Implementations/LibraryImportInternalWhisper.cs
+++ Whisper.net/Internals/Native/Implementations/LibraryImportInternalWhisper.cs
@@ -10,81 +10,79 @@ namespace Whisper.net.Internals.Native.Implementations;
 /// </summary>
 internal partial class LibraryImportInternalWhisper : INativeWhisper
 {
-    const string libraryName = "__Internal";
-
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial IntPtr whisper_init_from_file_with_params_no_state(string path, WhisperContextParams whisperContextParams);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial IntPtr whisper_init_from_buffer_with_params_no_state(IntPtr buffer, nuint buffer_size, WhisperContextParams whisperContextParams);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial void whisper_free(IntPtr context);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial void whisper_free_params(IntPtr paramsPtr);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial IntPtr whisper_full_default_params_by_ref(WhisperSamplingStrategy strategy);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial int whisper_full_with_state(IntPtr context, IntPtr state, WhisperFullParams parameters, IntPtr samples, int nSamples);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial int whisper_full_n_segments_from_state(IntPtr state);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial long whisper_full_get_segment_t0_from_state(IntPtr state, int index);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial long whisper_full_get_segment_t1_from_state(IntPtr state, int index);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial IntPtr whisper_full_get_segment_text_from_state(IntPtr state, int index);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial int whisper_full_n_tokens_from_state(IntPtr state, int index);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial float whisper_full_get_token_p_from_state(IntPtr state, int segmentIndex, int tokenIndex);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial int whisper_lang_max_id();
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial int whisper_lang_auto_detect_with_state(IntPtr context, IntPtr state, int offset_ms, int n_threads, IntPtr lang_probs);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial int whisper_pcm_to_mel_with_state(IntPtr context, IntPtr state, IntPtr samples, int nSamples, int nThreads);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial IntPtr whisper_lang_str(int lang_id);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial IntPtr whisper_init_state(IntPtr context);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial void whisper_free_state(IntPtr state);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial int whisper_full_lang_id_from_state(IntPtr state);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial void whisper_log_set(IntPtr logCallback, IntPtr user_data);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial void whisper_ctx_init_openvino_encoder_with_state(IntPtr context, IntPtr state, IntPtr modelPath, IntPtr device, IntPtr cacheDir);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial WhisperTokenData whisper_full_get_token_data_from_state(IntPtr state, int segmentIndex, int tokenIndex);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial IntPtr whisper_full_get_token_text_from_state(IntPtr context, IntPtr state, int segmentIndex, int tokenIndex);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial void ggml_log_set(IntPtr logCallback, IntPtr user_data);
 
-    [LibraryImport(libraryName, StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport(NativeConstants.InternalLibraryName, StringMarshalling = StringMarshalling.Utf8)]
     public static partial IntPtr whisper_print_system_info();
 
     public INativeWhisper.whisper_init_from_file_with_params_no_state Whisper_Init_From_File_With_Params_No_State => whisper_init_from_file_with_params_no_state;
