55f00453777dc0172451e29871077182ac4f4352
Sandro Hanea <sandro@shane.ro>
2024-12-26 16:44:29 +0100

    Test 2

--- tests/Whisper.net.Maui.Tests/Platforms/iOS/Program.cs
+++ tests/Whisper.net.Maui.Tests/Platforms/iOS/Program.cs
@@ -1,7 +1,8 @@
 // Licensed under the MIT license: https://opensource.org/licenses/MIT
 using UIKit;
+using Whisper.net.Maui.Tests.Platforms.iOS;
 
-namespace Whisper.net.Maui.Tests.Platforms.iOS;
+namespace Whisper.net.Maui.Tests;
 public class Program
 {
     // This is the main entry point of the application.
