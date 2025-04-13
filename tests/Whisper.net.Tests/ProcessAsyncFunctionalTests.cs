8f044ec20c58e1520affd20f03ec7ee8237aed95
Sandro Hanea <me@sandro.rocks>
2025-04-13 12:23:58 +0200

    Update async patterns and tests; bump xcode version to 16.3
    Improved `await using` usage in tests for better async handling and added a new parallel execution test. Fixed minor typos in test method names. Updated GitHub workflow to use Xcode 16.3 and made minor formatting adjustments for consistency.

--- tests/Whisper.net.Tests/ProcessAsyncFunctionalTests.cs
+++ tests/Whisper.net.Tests/ProcessAsyncFunctionalTests.cs
@@ -1,8 +1,10 @@
 // Licensed under the MIT license: https://opensource.org/licenses/MIT
 
+using Whisper.net.Wave;
 using Xunit;
 
 namespace Whisper.net.Tests;
+
 public partial class ProcessAsyncFunctionalTests(TinyModelFixture model) : IClassFixture<TinyModelFixture>
 {
     [Fact]
@@ -14,18 +16,18 @@ public partial class ProcessAsyncFunctionalTests(TinyModelFixture model) : IClas
 
         var encoderBegins = new List<EncoderBeginData>();
         using var factory = WhisperFactory.FromPath(model.ModelFile);
-        using var processor = factory.CreateBuilder()
-                        .WithLanguage("en")
-                        .WithEncoderBeginHandler((e) =>
-                        {
-                            encoderBegins.Add(e);
-                            return true;
-                        })
-                        .WithProgressHandler(progress.Add)
-                        .WithSegmentEventHandler(segments.Add)
-                        .Build();
+        await using var processor = factory.CreateBuilder()
+            .WithLanguage("en")
+            .WithEncoderBeginHandler((e) =>
+            {
+                encoderBegins.Add(e);
+                return true;
+            })
+            .WithProgressHandler(progress.Add)
+            .WithSegmentEventHandler(segments.Add)
+            .Build();
 
-        using var fileReader = await TestDataProvider.OpenFileStreamAsync("kennedy.wav");
+        await using var fileReader = await TestDataProvider.OpenFileStreamAsync("kennedy.wav");
         await foreach (var data in processor.ProcessAsync(fileReader))
         {
             segmentsEnumerated.Add(data);
@@ -40,7 +42,7 @@ public partial class ProcessAsyncFunctionalTests(TinyModelFixture model) : IClas
     }
 
     [Fact]
-    public async Task ProcessAsync_Cancelled_WillCancellTheProcessing_AndDispose_WillWaitUntilFullyFinished()
+    public async Task ProcessAsync_Cancelled_WillCancelTheProcessing_AndDispose_WillWaitUntilFullyFinished()
     {
         var segments = new List<SegmentData>();
         var segmentsEnumerated = new List<SegmentData>();
@@ -50,20 +52,20 @@ public partial class ProcessAsyncFunctionalTests(TinyModelFixture model) : IClas
         var encoderBegins = new List<EncoderBeginData>();
         using var factory = WhisperFactory.FromPath(model.ModelFile);
         var processor = factory.CreateBuilder()
-                        .WithLanguage("en")
-                        .WithEncoderBeginHandler((e) =>
-                        {
-                            encoderBegins.Add(e);
-                            return true;
-                        })
-                        .WithSegmentEventHandler(s =>
-                        {
-                            segments.Add(s);
-                            cts.Cancel();
-                        })
-                        .Build();
+            .WithLanguage("en")
+            .WithEncoderBeginHandler((e) =>
+            {
+                encoderBegins.Add(e);
+                return true;
+            })
+            .WithSegmentEventHandler(s =>
+            {
+                segments.Add(s);
+                cts.Cancel();
+            })
+            .Build();
 
-        using var fileReader = await TestDataProvider.OpenFileStreamAsync("kennedy.wav");
+        await using var fileReader = await TestDataProvider.OpenFileStreamAsync("kennedy.wav");
         try
         {
             await foreach (var data in processor.ProcessAsync(fileReader, cts.Token))
@@ -79,8 +81,8 @@ public partial class ProcessAsyncFunctionalTests(TinyModelFixture model) : IClas
         await processor.DisposeAsync();
 
         Assert.Empty(segmentsEnumerated);
-        Assert.Single( segments);
-        Assert.Single( encoderBegins);
+        Assert.Single(segments);
+        Assert.Single(encoderBegins);
         Assert.NotNull(taskCanceledException);
         Assert.Contains(segments, segmentData => segmentData.Text.Contains("nation should commit"));
     }
@@ -92,10 +94,10 @@ public partial class ProcessAsyncFunctionalTests(TinyModelFixture model) : IClas
 
         using var factory = WhisperFactory.FromPath(model.ModelFile);
         await using var processor = factory.CreateBuilder()
-                        .WithLanguage("en")
-                        .Build();
+            .WithLanguage("en")
+            .Build();
 
-        using var fileReader = await TestDataProvider.OpenFileStreamAsync("junkchunk16khz.wav");
+        await using var fileReader = await TestDataProvider.OpenFileStreamAsync("junkchunk16khz.wav");
         await foreach (var segment in processor.ProcessAsync(fileReader))
         {
             segments.Add(segment);
@@ -111,10 +113,10 @@ public partial class ProcessAsyncFunctionalTests(TinyModelFixture model) : IClas
 
         using var factory = WhisperFactory.FromPath(model.ModelFile);
         await using var processor = factory.CreateBuilder()
-                        .WithLanguage("en")
-                        .Build();
+            .WithLanguage("en")
+            .Build();
 
-        using var fileReader = await TestDataProvider.OpenFileStreamAsync("multichannel.wav");
+        await using var fileReader = await TestDataProvider.OpenFileStreamAsync("multichannel.wav");
         await foreach (var segment in processor.ProcessAsync(fileReader))
         {
             segments.Add(segment);
@@ -132,22 +134,22 @@ public partial class ProcessAsyncFunctionalTests(TinyModelFixture model) : IClas
 
         using var factory = WhisperFactory.FromPath(model.ModelFile);
         await using var processor = factory.CreateBuilder()
-                        .WithLanguage("en")
-                        .Build();
+            .WithLanguage("en")
+            .Build();
 
-        using var fileReader = await TestDataProvider.OpenFileStreamAsync("kennedy.wav");
+        await using var fileReader = await TestDataProvider.OpenFileStreamAsync("kennedy.wav");
         await foreach (var segment in processor.ProcessAsync(fileReader))
         {
             segments1.Add(segment);
         }
 
-        using var fileReader2 = await TestDataProvider.OpenFileStreamAsync("kennedy.wav");
+        await using var fileReader2 = await TestDataProvider.OpenFileStreamAsync("kennedy.wav");
         await foreach (var segment in processor.ProcessAsync(fileReader2))
         {
             segments2.Add(segment);
         }
 
-        using var fileReader3 = await TestDataProvider.OpenFileStreamAsync("kennedy.wav");
+        await using var fileReader3 = await TestDataProvider.OpenFileStreamAsync("kennedy.wav");
         await foreach (var segment in processor.ProcessAsync(fileReader3))
         {
             segments3.Add(segment);
@@ -155,5 +157,65 @@ public partial class ProcessAsyncFunctionalTests(TinyModelFixture model) : IClas
 
         Assert.True(segments1.SequenceEqual(segments2, new SegmentDataComparer()));
         Assert.True(segments2.SequenceEqual(segments3, new SegmentDataComparer()));
+    }
+
+    [Fact]
+    public async Task ProcessAsync_ParallelExecution_WillCompleteEverytime()
+    {
+        var segments1 = new List<SegmentData>();
+        var segments2 = new List<SegmentData>();
+        var segments3 = new List<SegmentData>();
+
+        using var factory = WhisperFactory.FromPath(model.ModelFile);
+        await using var fileReader = await TestDataProvider.OpenFileStreamAsync("kennedy.wav");
+        var waveParser = new WaveParser(fileReader);
+        var samples = await waveParser.GetAvgSamplesAsync();
+
+        var task1 = Task.Run(async () =>
+        {
+            await using var processor = factory.CreateBuilder()
+                .WithLanguage("en")
+                .Build();
+
+            await foreach (var segment in processor.ProcessAsync(samples))
+            {
+                Thread.Sleep(300);
+                segments1.Add(segment);
+            }
+        });
+
+        var task2 = Task.Run(async () =>
+        {
+            await using var processor = factory.CreateBuilder()
+                .WithLanguage("en")
+                .Build();
+
+            await foreach (var segment in processor.ProcessAsync(samples))
+            {
+                Thread.Sleep(300);
+                segments2.Add(segment);
+            }
+        });
+
+
+        var task3 = Task.Run(async () =>
+        {
+            await using var processor = factory.CreateBuilder()
+                .WithLanguage("en")
+                .Build();
+
+            await foreach (var segment in processor.ProcessAsync(samples))
+            {
+                Thread.Sleep(300);
+                segments3.Add(segment);
+            }
+        });
+
+        await Task.WhenAll(task1, task2, task3);
+
+        // Assert
+        Assert.True(segments1.SequenceEqual(segments2, new SegmentDataComparer()));
+        Assert.True(segments2.SequenceEqual(segments3, new SegmentDataComparer()));
+        Assert.True(segments1.SequenceEqual(segments3, new SegmentDataComparer()));
     }
 }
