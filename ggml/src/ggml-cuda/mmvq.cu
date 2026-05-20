0a0a34287e84641c34679c1a38fed299a56e9d4b
ravel7524 <58877666+ravel7524@users.noreply.github.com>
2026-05-20 03:52:21 +0200

    ggml-cuda: tune RDNA3 Q6_K MMVQ nwarps (llama/23349)

--- ggml/src/ggml-cuda/mmvq.cu
+++ ggml/src/ggml-cuda/mmvq.cu
@@ -359,7 +359,9 @@ static constexpr __host__ __device__ int calc_nwarps(ggml_type type, int ncols_d
                 case GGML_TYPE_Q5_1:
                 case GGML_TYPE_Q8_0:
                 case GGML_TYPE_Q4_K:
+                    return 8;
                 case GGML_TYPE_Q6_K:
+                    return 2;
                 case GGML_TYPE_IQ4_NL:
                     return 8;
                 default:
